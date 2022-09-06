6044333772ff3d56a110797dd689ee8119b25e83
Neeraj7979 <Imnsinha77@outlook.com>
2022-09-06 09:59:53 +0530

    Created method is prime number using lambda expression and run all the test cases.

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -1,9 +1,20 @@
 package com.jap.prime;
 
-public class PrimeCheckerImpl {
+public class PrimeCheckerImpl{
     public boolean primeNumberCheck(int number) {
         // write the logic here to check is a number is prime or not
-        return false;
-    }
+        PrimeChecker p1= number1 -> { boolean flag = false;
+            for (int i = 2; i < number; i++) {
+                if (number % 2 == 0)
+                    flag = false;
+                else {
+                    flag = true;
+                }
+            }
+            return flag;
+        };
+        return p1.isPrime(number);
 
+    }
 }
+
